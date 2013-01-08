cb6b320dc05b7c81bf392a1924ed2b01a00c26eb
Maurizio Lupo <maurizio.lupo@gmail.com>
2013-01-08 14:04:44 +0100

    first release, mostly untested

--- backbone.occamsrazor.js
+++ backbone.occamsrazor.js
@@ -1,49 +1,133 @@
 /*
-This modules enhances Backbone.js + Marionettejs with occamsrazor.js
+This modules enhances Backbone.js using occamsrazor.js
+It allows to use heterogeneous and pluggable collections.
 */
 
 (function (Backbone){
-/*
-Plugins entry points
-*/
+"use strict";
 
 Backbone.Occamsrazor = {};
 
-// get a model from a simple Javascript object:
-// Backbone.Occamsrazor.getModel(obj)
-Backbone.Occamsrazor.getModel = occamsrazor();
-
-// sync an object with an appropriate function 
-Backbone.Occamsrazor.sync = occamsrazor().add(null, Backbone.sync);
-
 /*
-Backbone.Occamsrazor.Model
-*/
+Backbone.Occamsrazor.Collection 
 
-Backbone.Occamsrazor.Model = Backbone.Model.extend({
-    sync: Backbone.Occamsrazor.sync
-});
-
-/*
-Backbone.occamsrazor.Collection 
+A collection for heterogeneous elements
 */
 
 Backbone.Occamsrazor.Collection = Backbone.Collection.extend({
-    // replacing the boring factory function with a more exciting adapter registry
-    model: Backbone.Occamsrazor.getModel,
-    sync: Backbone.Occamsrazor.sync
+    // replacing the boring constructor function with a more exciting adapter registry
+    model: occamsrazor()
 });
 
 /*
-Backbone.Occamsrazor.ItemView (a view for a single item)
+Backbone.Occamsrazor.ItemView
+
+A very simple view for rendering a model (passed in the constructor)
+
+remember to call Backbone.View.prototype.initialize.call(this, [options])
+if you override the initialize
 */
 
-Backbone.Occamsrazor.ItemView = Backbone.Marionette.ItemView.extend({
-  template: occamsrazor()
+Backbone.Occamsrazor.ItemView = Backbone.View.extend({
+    initialize : function (options, model){
+        this.model = model;
+    }
+});
+
+/*
+Backbone.Occamsrazor.CollectionView
+
+A very simple view that renders each item with an appropriate view
+*/
+
+Backbone.Occamsrazor.CollectionView = Backbone.View.extend({
+    itemView: occamsrazor(),
+    viewOptions: {},
+    initialize: function (){
+        this.children = {};
+        this.initialEvents();
+    },
+    initialEvents: function (){
+        if (this.collection){
+          this.listenTo(this.collection, "add", this.addChildView);
+          this.listenTo(this.collection, "remove", this.removeChildView);
+          this.listenTo(this.collection, "reset", this.render);
+        }
+    },
+    
+    render: function (){
+        _.each(this.children, function (view){
+            view.remove();
+        });
+
+        this.children = {};
+
+        var that = this;
+        this.collection.each(function(item, index){
+            var options = _.extend({index: index}, that.viewOptions);
+            that.addChildView(item, that.collection, options);
+        });
+        return this;
+    },
+    
+
+    addChildView: function (item, collection, options){
+        var index;
+        if (options && options.index){
+            index = options.index;
+        }
+        else {
+            index = collection.indexOf(item);
+            if (index === -1){
+                index = 0;
+            }
+        }
+
+        var view = new this.itemView(options, item);
+        view.render();
+
+        this.children[item.cid] = view;
+        this.addToView(view, index);
+        
+    },
+    removeChildView: function (item){
+        this.children[item.cid].remove();
+        delete this.children[item.cid];
+    },
+
+    addToView: function (view, index){
+        var $children = this.$el.children();
+        if ($children.length === 0 || $children.length >= index ){
+            this.$el.append(view.el);
+        }
+        $children.eq(index).before(view.el);
+    }
+
+});
+
+/*
+
+You could extend Marionette.CollectionView in the same way:
+
+
+var MyView = Backbone.Marionette.ItemView.extend({
+  template: "#some-template",
+    initialize : function (options, model){
+        this.model = model;
+    }
 });
 
 
+var MyCollectionView = Backbone.Marionette.CollectionView.extend({
+  itemView: occamsrazor(),
+  buildItemView: function(item, ItemViewType, itemViewOptions){
+    var options = _.extend({model: item}, itemViewOptions);
+    var view = new ItemViewType(options, item);
+    return view;
+  },  
+});
 
+*/
 
 
 }(Backbone));
--- tests/test.js
+++ tests/test.js
@@ -6,25 +6,157 @@ Build a very simple infrastructure
 /*
 
 */
-module("", {
+module("Heterogeneous collection", {
     setup: function (){
-        var Guitar = Backbone.Model.extend();
-    
-    
-        Backbone.Occamsrazor.getModel.add(,Guitar);
 
+        /*
+        validators
+        */
+
+        var hasWidth = function (obj){
+                if (obj instanceof Backbone.Model){
+                    return obj.has('width');
+                }
+                return 'width' in obj;
+            },
+            hasHeight = function (obj){
+                if (obj instanceof Backbone.Model){
+                    return obj.has('height');
+                }
+                return 'height' in obj;
+            },
+            hasRadius = function (obj){
+                if (obj instanceof Backbone.Model){
+                    return obj.has('radius');
+                }
+                return 'radius' in obj;
+            },
+            hasWidthHeight = occamsrazor.chain(hasWidth, hasHeight);
+
+        /*
+        collection
+        */
+
+        this.ShapesCollection = Backbone.Occamsrazor.Collection.extend({
+            localStorage: new Backbone.LocalStorage("testcollection")
+        });
+
+        this.shapesCollection = new this.ShapesCollection;
+
+        /*
+        models
+        */
+
+        this.Square = Backbone.Model.extend({
+            getArea: function (){
+                var w = this.get('width');
+                return w*w;
+            }
+        });
+
+        this.Rectangle = Backbone.Model.extend({
+            getArea: function (){
+                var w = this.get('width'),
+                    h = this.get('height');
+                return w*h;
+            }
+        });
+
+        this.Circle = Backbone.Model.extend({
+            getArea: function (){
+                var r = this.get('radius');
+                return Math.PI * r * r;
+            }
+        });
+
+        this.rect = {width: 10, height: 5};
+        this.square = {width: 10};
+        this.circle = {radius: 3};
+
+
+        /*
+        add 3 kind of models to the collection
+        */
+
+        this.shapesCollection.model.addConstructor(hasWidth, this.Square);
+        this.shapesCollection.model.addConstructor(hasWidthHeight, this.Rectangle);
+        this.shapesCollection.model.addConstructor(hasRadius, this.Circle);
+
+
+        /*
+        add 3 objects to the collection
+        */
+
+        this.shapesCollection.add([this.rect, this.square, this.circle]);
+
+
+        /*
+        collection view
+        */
+
+        this.shapesView = new Backbone.Occamsrazor.CollectionView({collection: this.shapesCollection, el: $('#fixture')});
+
+        /*
+        add 3 kind of "item view" to the collection view
+        */
+
+
+        this.shapesView.itemView.addConstructor([null, hasRadius], Backbone.Occamsrazor.ItemView.extend({
+            tagName:  'div',
+            render: function (){
+                this.$el.html('The area of the circle is ' + this.model.getArea());
+                return this;
+                
+            }
+        }));
+
+        this.shapesView.itemView.addConstructor([null, hasWidthHeight], Backbone.Occamsrazor.ItemView.extend({
+            tagName:  'div',
+            render: function (){
+                this.$el.html('The area of the square is ' + this.model.getArea());
+                return this;
+            }
+        }));
+
+        this.shapesView.itemView.addConstructor([null, hasWidth], Backbone.Occamsrazor.ItemView.extend({
+            tagName:  'div',
+            render: function (){
+                this.$el.html('The area of the rectangle is ' + this.model.getArea());
+                return this;
+            }
+        }));
+
+    },
+    teardown: function (){
+        // empty localstorage
+        while (localStorage.length) localStorage.removeItem(localStorage.key(0));
     }
 });
 
-
-test("", function() {
+test("Test add collection", function() {
+    ok(this.shapesCollection.at(0) instanceof this.Rectangle,'shape 1 is a rectangle');
+    ok(this.shapesCollection.at(1) instanceof this.Square,'shape 2 is a square');
+    ok(this.shapesCollection.at(2) instanceof this.Circle,'shape 3 is a circle');
 });
 
-
-var has_name = occamsrazor.validator(BackboneOR.validators.isAnything, function (obj){
-    return obj.get('name');
+test("Test save and retrieve model collection", function() {
+    this.shapesCollection.each(function (item){
+        item.save();
+    });
+    this.shapesCollection.reset();
+    ok(this.shapesCollection.length === 0,'the collection is empty');
+    this.shapesCollection.fetch();
+    ok(this.shapesCollection.length === 3,'the collection is full');
+    
+    ok(this.shapesCollection.at(0) instanceof this.Rectangle,'shape 1 is a rectangle');
+    ok(this.shapesCollection.at(1) instanceof this.Square,'shape 2 is a square');
+    ok(this.shapesCollection.at(2) instanceof this.Circle,'shape 3 is a circle');
 });
 
+test("Test view", function() {
+//    this.shapesView.render();
+//    ok(this.shapesCollection.at(0) instanceof this.Rectangle,'shape 1 is a rectangle');
+});
 
 
 
